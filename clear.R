ceb88fe5235d7a8f1d9e30a3c369b0ed98312b8f
Yuelin Jiang <gloriajiang715@gmail.com>
2020-11-11 16:08:43 +0700

    update

--- clear.R
+++ clear.R
@@ -1,16 +1,17 @@
 library(tidyverse)
 library(magrittr)
-femaRaw <- read.csv("PublicAssistanceFundedProjectsDetails.csv", col.names = TRUE)
+library(lubridate)
+femaRaw <- read.csv("PublicAssistanceFundedProjectsDetails.csv")
 
-hurricane <- femaRaw %>% dplyr::select(disasterNumber, declarationDate, incidentType, pwNumber, damageCategory, projectSize, county, countyCode, stateCode, stateNumberCode, projectAmount, federalShareObligated, totalObligated)  
+hurricane <- femaRaw %<>% dplyr::select(disasterNumber, declarationDate, incidentType, pwNumber, damageCategory, projectSize, county, countyCode, state, stateCode, stateNumberCode, projectAmount, federalShareObligated, totalObligated)  
 
 
 hurricane <- femaRaw %>% dplyr::filter(incidentType == "Hurricane")  
 hurricane %<>% separate(declarationDate, c('date', 'time'), sep = "T")
-hurricane %<>% mutate(year = date[0:4])
-hurricane %<>% separate(date, c('yyyy', 'mm', 'dd'), sep = '-')
-hurricane$yyyy <- as.numeric(hurricane$yyyy)
-hurricane %<>% filter(yyyy > 2008)
-hurricane %<>% select(-c(mm, dd, time, incidentType))
+
+hurricane$date <- as.Date(hurricane$date, format = "%Y-%m-%d")
+
+hurricane %<>% dplyr::filter(year(hurricane$date) > 2008)
+hurricane %<>% select(-c(time, incidentType))
 
 write.csv(hurricane, "hurricane.csv")
